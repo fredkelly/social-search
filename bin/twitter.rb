0a495ace3cc448ac4124613ff678fe4b07d832dd
Fred Kelly <me@fredkelly.net>
2012-10-01 15:16:37 +0100

    Added storing of results arrays.

--- bin/twitter.rb
+++ bin/twitter.rb
@@ -1,10 +1,29 @@
 module Twitter
+  # Overrides to allow > 100 results,
+  # as well as storing/loading from file.
   def self.search(q, options = {})
     results = []
-    options[:rpp] ||= 100
-    for page in 1..(options[:rpp]/100)
-      results += super(q, options.merge(:page => page, :rpp => 100)).results
+    results_file = "./stored/#{options[:file]}.tweets"
+    
+    begin
+      # try reading from stored
+      results = Marshal.load File.read(results_file)
+    rescue
+      # get results from twitter
+      options[:rpp] ||= 100
+      for page in 1..(options[:rpp]/100)
+        results += super(q, options.merge(:page => page, :rpp => 100)).results
+      end
     end
+    
+    # save to file?
+    unless options[:file].nil?
+      File.open(results_file, 'w') do |file|
+        file.write Marshal.dump(results)
+      end
+    end
+    
+    # return Tweets
     results
   end
   
--- social_search.rb
+++ social_search.rb
@@ -1,6 +1,7 @@
 class SocialSearch < Sinatra::Base
   get '/' do
-    results = Twitter.search('kate middleton', :rpp => 1000, :result_type => :recent)
+    #results = Twitter.search('kate middleton', :rpp => 1000, :result_type => :recent, :file => 'test')
+    results = Twitter.search('', :file => 'test')
     puts results.size
     results.join('<br />')
   end
