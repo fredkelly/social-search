a393da925d1f81ee7978a6a0a7bd6b9f0cb5f8e0
Freddy Kelly <me@fredkelly.net>
2012-11-20 11:23:18 +0000

    Removed array split from description.

--- bin/page.rb
+++ bin/page.rb
@@ -5,7 +5,7 @@ end
 
 class Nokogiri::HTML::Document
   def description
-    # meta description, or first 100 words
-    (css('meta[name=\'description\']').first['content'] rescue at('body').inner_text[0..100]).chomp
+    # meta description, or document text
+    (css('meta[name=\'description\']').first['content'] rescue at('body').inner_text).chomp
   end
 end
