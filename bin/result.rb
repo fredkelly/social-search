bfc32d64ca8dcaebd70d31bf58f7f6c1bbc54b1d
Fred Kelly <me@fredkelly.net>
2012-11-20 16:55:22 +0000

    Updated exception truncate method.

--- bin/result.rb
+++ bin/result.rb
@@ -3,7 +3,11 @@
 # the user when they complete a search.
 class Result < Struct.new(:title, :url, :description)
   # truncated description
-  def excerpt
-    description.split[0..50].join(' ') + '...'
+  def excerpt(length = 50)
+    if description.size > length
+      description.split[0..length].join(' ') + '..'
+    else
+      description
+    end
   end
 end
