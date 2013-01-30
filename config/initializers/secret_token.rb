d4d6f413edfb2984e7813d942b2fc1eae21c3f74
Fred Kelly <me@fredkelly.net>
2013-01-30 14:32:49 +0000

    Blank Rails App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialSearch::Application.config.secret_token = '10fb757e76f47d277754520179573a33c52fff525750bd029b5dfc3c08ff34a54c5c5b5ae125e259d002f8c59fef49bb852f043e2308bca90805cb1b1ccdf456'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SocialSearch::Application.config.session_store :cookie_store, key: '_social-search_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SocialSearch::Application.config.session_store :active_record_store
