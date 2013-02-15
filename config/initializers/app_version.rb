88395d11b22dba94a6975b62468be05a64696047
Fred Kelly <me@fredkelly.net>
2013-02-15 18:09:49 +0000

    Updated app_version to use custom buildpack.

--- config/initializers/app_version.rb
+++ config/initializers/app_version.rb
@@ -1,5 +1,6 @@
 module SocialSearch
   class Application
-    VERSION ||= %x(git rev-parse HEAD).chomp
+    # using custom buildback from Heroku (if prod)
+    VERSION ||= Rails.env.production? ? ENV['SHA'] : %x(git rev-parse HEAD).chomp
   end
 end
