ce42330bdb848f4c6b69a0356104d3cd55b51a2c
Fred Kelly <me@fredkelly.net>
2013-02-15 18:58:58 +0000

    Removed remaining instance of APP_VERSION (replaced with FILE_VERSION).

--- app/models/session.rb
+++ app/models/session.rb
@@ -6,7 +6,7 @@ class Session < ActiveRecord::Base
   # Generates or retrieves instance based
   # on supplied <tt>ActionDispatch::Request</tt>.
   def self.find_or_create_by_request(request)
-    where(session_id: request.session[:session_id], app_version: Rails.application.class::VERSION).first_or_initialize.tap do |session|
+    where(session_id: request.session[:session_id], app_version: Rails.application.class::FILE_VERSION).first_or_initialize.tap do |session|
       [:accept, :accept_charset, :accept_language, :remote_addr, :user_agent].each do |key|
         session[key] = request.send(key)
       end
