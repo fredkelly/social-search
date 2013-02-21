b33d6053dec40809f940b206ec3d916972162bcc
Freddy Kelly <me@fredkelly.net>
2013-02-21 21:44:12 +0000

    reverted to custom.log for time being

--- config/initializers/custom_logger.rb
+++ config/initializers/custom_logger.rb
@@ -1,18 +1,6 @@
-# log to STDOUT for heroku so we can grep for [CUSTOM]
-class HerokuLogger < ActiveSupport::BufferedLogger
-  def initialize
-    super(Rails.env.production? ? $stdout : Rails.root.join('log/custom.log'))
-  end
-  
-  def add(severity, message = nil, progname = nil, &block)
-    mesage.gsub!("\n", "\n[CUSTOM]") if Rails.env.production?
-    super(severity, message, progname, &block)
-  end
-end
+CustomLogger = ActiveSupport::BufferedLogger.new(Rails.root.join('log/custom.log'))
 
 # add colour methods to String
 class String
   include Term::ANSIColor
 end
-
-CustomLogger = HerokuLogger.new
