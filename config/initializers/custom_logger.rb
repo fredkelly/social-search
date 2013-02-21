309063dd84847c1440a770aaf47658b59ea60d5a
Freddy Kelly <me@fredkelly.net>
2013-02-21 20:44:14 +0000

    Set up custom_logger to override Rails default in prod.

--- config/initializers/custom_logger.rb
+++ config/initializers/custom_logger.rb
@@ -1,4 +1,9 @@
-CustomLogger = ActiveSupport::BufferedLogger.new(Rails.root.join('log/custom.log'))
+CustomLogger = ActiveSupport::BufferedLogger.new(Rails.env.production? ? Rails.root.join('log/custom.log') : $STDOUT)
+
+# disable all logging for demonstration
+if Rails.env.production?
+  ActiveRecord::Base.logger = nil
+end
 
 # add colour methods to String
 class String
