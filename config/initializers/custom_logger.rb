22136eb3217a5abdeca1cafbe2ba35fdd6e11724
Fred Kelly <me@fredkelly.net>
2013-03-16 19:21:48 +0000

    removed dependency on clusterer

--- config/initializers/custom_logger.rb
+++
@@ -1,6 +0,0 @@
-CustomLogger = ActiveSupport::BufferedLogger.new(Rails.root.join('log/custom.log'))
-
-# add colour methods to String
-class String
-  include Term::ANSIColor
-end
--- config/initializers/enumerable.rb
+++
@@ -1,5 +0,0 @@
-Enumerable.class_eval do
-  def mode
-    group_by{|e| e}.values.max_by(&:size).first
-  end
-end
