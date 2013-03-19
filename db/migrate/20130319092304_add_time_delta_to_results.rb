415aeafde2270448a21fa2be5acef439b18591ab
Fred Kelly <me@fredkelly.net>
2013-03-19 15:33:15 +0000

    removed Result.update in migration

--- db/migrate/20130319092304_add_time_delta_to_results.rb
+++ db/migrate/20130319092304_add_time_delta_to_results.rb
@@ -1,10 +1,5 @@
 class AddTimeDeltaToResults < ActiveRecord::Migration
   def change
     add_column :results, :time_delta, :float
-    
-    Result.find_each do |r|
-      r.update_attribute(:time_delta, r.created_at)
-      r.save
-    end
   end
 end
