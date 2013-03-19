476e477edb64909fdfb63f33e14b7a978191afd1
Fred Kelly <me@fredkelly.net>
2013-03-19 15:11:07 +0000

    updated time_delta migration to repair previous data

--- db/migrate/20130319092304_add_time_delta_to_results.rb
+++ db/migrate/20130319092304_add_time_delta_to_results.rb
@@ -1,5 +1,10 @@
 class AddTimeDeltaToResults < ActiveRecord::Migration
   def change
     add_column :results, :time_delta, :float
+    
+    Result.find_each do |r|
+      r.time_delta ||= r.created_at
+      r.save
+    end
   end
 end
