55e98ed112ef00a63c9a2ed7f838450c69c1eccc
Fred Kelly <me@fredkelly.net>
2013-03-20 09:46:03 +0000

    added default value to time_delta

--- db/migrate/20130319092304_add_time_delta_to_results.rb
+++ db/migrate/20130319092304_add_time_delta_to_results.rb
@@ -1,11 +1,5 @@
 class AddTimeDeltaToResults < ActiveRecord::Migration
   def change
-    add_column :results, :time_delta, :float
-    
-    # quick fix for old results
-    now = Time.now
-    Result.find_each do |r|
-      r.update_attribute(:time_delta, now - r.created_at) if r.read_attribute(:time_delta).nil?
-    end
+    add_column :results, :time_delta, :float, default: 0.0
   end
 end
